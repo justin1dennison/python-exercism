97b3e9f51719e6c8666ff953cab8b19243de225b
Justin Dennison <justin1dennison@gmail.com>
2019-08-10 21:01:40 -0400

    updated matrix to not allow mutation and be more specific about splitting

--- matrix/matrix.py
+++ matrix/matrix.py
@@ -1,12 +1,12 @@
 class Matrix(object):
     def __init__(self, matrix_string):
         self._matrix = [
-                [int(n) for n in row.split(" ")] 
-                for row in matrix_string.split("\n")
+                [int(n) for n in row.split()] 
+                for row in matrix_string.splitlines()
             ]
 
     def row(self, index):
-        return self._matrix[index - 1]
+        return [element for element in self._matrix[index - 1]]
 
     def column(self, index):
         return [r[index - 1] for r in self._matrix]
