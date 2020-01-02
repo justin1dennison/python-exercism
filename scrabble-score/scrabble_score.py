9fbe63addca7bf7d856bd7126ee039b24beb4c70
Justin Dennison <justin1dennison@gmail.com>
2020-01-02 11:26:17 -0500

    used dictionaries

--- scrabble-score/scrabble_score.py
+++ scrabble-score/scrabble_score.py
@@ -1,4 +1,4 @@
-letter_values = [
+scores = (
     (set("aeioulnrst"), 1),
     (set("dg"), 2),
     (set("bcmp"), 3),
@@ -6,8 +6,10 @@ letter_values = [
     (set("k"), 5),
     (set("jx"), 8),
     (set("qz"), 10),
-]
+)
+
+lookup = dict((letter, value) for letters, value in scores for letter in letters)
 
 
 def score(word):
-    return sum(value for letter in word for st, value in letter_values if letter.lower() in st)
+    return sum(lookup[letter] for letter in word.lower())
